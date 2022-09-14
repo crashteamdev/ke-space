513c62d8a99a44b95231ec64a0e401c543207f6d
vitaxa <vitaxa93gamebox@gmail.com>
2022-09-14 23:26:35 +0300

    fix user creation filter [3]

--- src/main/kotlin/dev/crashteam/repricer/config/security/UserCreationFilter.kt
+++ src/main/kotlin/dev/crashteam/repricer/config/security/UserCreationFilter.kt
@@ -3,31 +3,30 @@ package dev.crashteam.repricer.config.security
 import dev.crashteam.repricer.repository.postgre.AccountRepository
 import dev.crashteam.repricer.repository.postgre.entity.AccountEntity
 import kotlinx.coroutines.reactor.awaitSingle
-import kotlinx.coroutines.reactor.awaitSingleOrNull
-import kotlinx.coroutines.runBlocking
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import org.springframework.web.server.ServerWebExchange
 import org.springframework.web.server.WebFilter
 import org.springframework.web.server.WebFilterChain
 import reactor.core.publisher.Mono
-import reactor.core.publisher.toMono
 import java.security.Principal
 
 @Component
 class UserCreationFilter(
     private val accountRepository: AccountRepository
 ) : WebFilter {
-    override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> = runBlocking {
-        val principal = exchange.getPrincipal<Principal>().awaitSingleOrNull()
-        if (principal?.name == null) {
-            exchange.response.rawStatusCode = HttpStatus.UNAUTHORIZED.value()
-            return@runBlocking exchange.response.setComplete().awaitSingle()
+    override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> {
+        return exchange.getPrincipal<Principal>().flatMap {
+            if (it?.name == null) {
+                exchange.response.rawStatusCode = HttpStatus.UNAUTHORIZED.value()
+                exchange.response.setComplete()
+            } else {
+                val accountEntity = accountRepository.getAccount(it.name)
+                if (accountEntity == null) {
+                    accountRepository.save(AccountEntity(userId = it.name))
+                }
+                chain.filter(exchange)
+            }
         }
-        val accountEntity = accountRepository.getAccount(principal.name)
-        if (accountEntity == null) {
-            accountRepository.save(AccountEntity(userId = principal.name))
-        }
-        return@runBlocking chain.filter(exchange).awaitSingle()
-    }.toMono()
+    }
 }
