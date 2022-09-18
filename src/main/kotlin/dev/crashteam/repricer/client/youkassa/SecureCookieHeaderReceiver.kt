4d6ae8622c95d79aa4765bedd82c9e222ae6b9b7
vitaxa <vitaxa93gamebox@gmail.com>
2022-09-18 19:27:13 +0300

    revert commit "remove switch to signin page"

--- src/main/kotlin/dev/crashteam/repricer/client/youkassa/SecureCookieHeaderReceiver.kt
+++ src/main/kotlin/dev/crashteam/repricer/client/youkassa/SecureCookieHeaderReceiver.kt
@@ -59,9 +59,9 @@ class SecureCookieHeaderReceiver(
                 Thread.sleep(Random().nextLong(600, 1200))
                 val newTabName = (webDriver.windowHandles as HashSet).toArray()[1].toString()
                 webDriver.switchTo().window(newTabName)
-//                webDriverWait.until { ExpectedConditions.presenceOfElementLocated(By.cssSelector("a[href=\"/seller/signin\"]")) }
-//                webDriver.findElement(By.cssSelector("a[href=\"/seller/signin\"]")).click() // Click on signin button
-//                webDriverWait.until { ExpectedConditions.presenceOfElementLocated(By.cssSelector("button.solid")) }
+                webDriverWait.until { ExpectedConditions.presenceOfElementLocated(By.cssSelector("a[href=\"/seller/signin\"]")) }
+                webDriver.findElement(By.cssSelector("a[href=\"/seller/signin\"]")).click() // Click on signin button
+                webDriverWait.until { ExpectedConditions.presenceOfElementLocated(By.cssSelector("button.solid")) }
 
                 val qratorJsIdCookie = webDriver.manage().getCookieNamed("qrator_jsid")
                 userCookieRepository.saveCookie(
