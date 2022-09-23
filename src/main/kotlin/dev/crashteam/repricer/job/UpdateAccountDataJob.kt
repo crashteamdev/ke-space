cd8cee98e662a18e1fa1d5ef8b60e6806532b5ee
vitaxa <vitaxa93gamebox@gmail.com>
2022-09-23 21:19:25 +0300

    retry update execute

--- src/main/kotlin/dev/crashteam/repricer/job/UpdateAccountDataJob.kt
+++ src/main/kotlin/dev/crashteam/repricer/job/UpdateAccountDataJob.kt
@@ -1,8 +1,6 @@
 package dev.crashteam.repricer.job
 
 import dev.crashteam.repricer.client.ke.KazanExpressLkClient
-import dev.crashteam.repricer.client.ke.model.StyxResponse
-import dev.crashteam.repricer.client.ke.model.lk.CheckTokenResponse
 import dev.crashteam.repricer.db.model.enums.UpdateState
 import dev.crashteam.repricer.extensions.getApplicationContext
 import dev.crashteam.repricer.repository.postgre.KeAccountRepository
@@ -10,6 +8,7 @@ import dev.crashteam.repricer.service.KazanExpressSecureService
 import dev.crashteam.repricer.service.UpdateKeAccountService
 import mu.KotlinLogging
 import org.quartz.JobExecutionContext
+import org.springframework.retry.support.RetryTemplate
 import org.springframework.scheduling.quartz.QuartzJobBean
 import org.springframework.transaction.PlatformTransactionManager
 import org.springframework.transaction.support.TransactionTemplate
@@ -27,28 +26,37 @@ class UpdateAccountDataJob : QuartzJobBean() {
         val transactionManager = applicationContext.getBean(PlatformTransactionManager::class.java)
         val kazanExpressSecureService = applicationContext.getBean(KazanExpressSecureService::class.java)
         val kazanExpressLkClient = applicationContext.getBean(KazanExpressLkClient::class.java)
+        val retryTemplate = applicationContext.getBean(RetryTemplate::class.java)
         val userId = context.jobDetail.jobDataMap["userId"] as? String
             ?: throw IllegalStateException("userId can't be null")
         val keAccountId = context.jobDetail.jobDataMap["keAccountId"] as? UUID
             ?: throw IllegalStateException("keAccountId can't be null")
         try {
-            log.info { "Execute update ke account job. userId=$userId;keAccountId=$keAccountId" }
-            keAccountRepository.changeUpdateState(userId, keAccountId, UpdateState.in_progress)
-            TransactionTemplate(transactionManager).execute {
-                val accessToken = kazanExpressSecureService.authUser(userId, keAccountId)
-                val checkToken = kazanExpressLkClient.checkToken(userId, accessToken).body!!
-                val kazanExpressAccount = keAccountRepository.getKazanExpressAccount(userId, keAccountId)!!.copy(
-                    externalAccountId = checkToken.accountId,
-                    name = checkToken.firstName,
-                    email = checkToken.email
-                )
-                keAccountRepository.save(kazanExpressAccount)
-                log.info { "Update shops. userId=$userId;keAccountId=$keAccountId" }
-                updateKeAccountService.updateShops(userId, keAccountId)
-                log.info { "Update shop items. userId=$userId;keAccountId=$keAccountId" }
-                updateKeAccountService.updateShopItems(userId, keAccountId)
-                log.info { "Change update state to finished. userId=$userId;keAccountId=$keAccountId" }
-                keAccountRepository.changeUpdateState(userId, keAccountId, UpdateState.finished, LocalDateTime.now())
+            retryTemplate.execute<Void, Exception> {
+                log.info { "Execute update ke account job. userId=$userId;keAccountId=$keAccountId" }
+                keAccountRepository.changeUpdateState(userId, keAccountId, UpdateState.in_progress)
+                TransactionTemplate(transactionManager).execute {
+                    val accessToken = kazanExpressSecureService.authUser(userId, keAccountId)
+                    val checkToken = kazanExpressLkClient.checkToken(userId, accessToken).body!!
+                    val kazanExpressAccount = keAccountRepository.getKazanExpressAccount(userId, keAccountId)!!.copy(
+                        externalAccountId = checkToken.accountId,
+                        name = checkToken.firstName,
+                        email = checkToken.email
+                    )
+                    keAccountRepository.save(kazanExpressAccount)
+                    log.info { "Update shops. userId=$userId;keAccountId=$keAccountId" }
+                    updateKeAccountService.updateShops(userId, keAccountId)
+                    log.info { "Update shop items. userId=$userId;keAccountId=$keAccountId" }
+                    updateKeAccountService.updateShopItems(userId, keAccountId)
+                    log.info { "Change update state to finished. userId=$userId;keAccountId=$keAccountId" }
+                    keAccountRepository.changeUpdateState(
+                        userId,
+                        keAccountId,
+                        UpdateState.finished,
+                        LocalDateTime.now()
+                    )
+                    null
+                }
             }
         } catch (e: Exception) {
             log.warn(e) { "Failed to update account data for userId=$userId;keAccountId=$keAccountId"  }
