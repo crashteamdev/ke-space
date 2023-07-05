476823cf4c3d19eebef29036a90dda88d5484765
SageFreke <lenigrast@gmail.com>
2023-07-05 10:45:42 +0300

    add new strategy to converter

--- src/main/kotlin/dev/crashteam/repricer/converter/KeAccountShopItemStrategyConverter.kt
+++ src/main/kotlin/dev/crashteam/repricer/converter/KeAccountShopItemStrategyConverter.kt
@@ -1,6 +1,7 @@
 package dev.crashteam.repricer.converter
 
 import dev.crashteam.openapi.kerepricer.model.CloseToMinimalStrategy
+import dev.crashteam.openapi.kerepricer.model.EqualPriceStrategy
 import dev.crashteam.openapi.kerepricer.model.KeAccountShopItemStrategy
 import dev.crashteam.openapi.kerepricer.model.QuantityDependentStrategy
 import dev.crashteam.openapi.kerepricer.model.Strategy
@@ -8,7 +9,7 @@ import dev.crashteam.repricer.db.model.enums.StrategyType
 import dev.crashteam.repricer.repository.postgre.entity.strategy.KazanExpressAccountShopItemStrategyEntity
 
 class KeAccountShopItemStrategyConverter: DataConverter<KazanExpressAccountShopItemStrategyEntity, KeAccountShopItemStrategy> {
-    override fun convert(source: KazanExpressAccountShopItemStrategyEntity): KeAccountShopItemStrategy? {
+    override fun convert(source: KazanExpressAccountShopItemStrategyEntity): KeAccountShopItemStrategy {
         return KeAccountShopItemStrategy().apply {
             id = source.id
             strategy = getStrategy(source)
@@ -21,6 +22,8 @@ class KeAccountShopItemStrategyConverter: DataConverter<KazanExpressAccountShopI
                 source.minimumThreshold?.toDouble(), source.maximumThreshold?.toDouble())
             StrategyType.quantity_dependent.name -> QuantityDependentStrategy(source.step, source.strategyType,
                 source.minimumThreshold?.toDouble(), source.maximumThreshold?.toDouble())
+            StrategyType.equal_price.name -> EqualPriceStrategy(source.strategyType, source.minimumThreshold?.toDouble(),
+                source.maximumThreshold?.toDouble())
             else -> throw IllegalArgumentException("No such strategy - ${source.strategyType}")
         }
     }
